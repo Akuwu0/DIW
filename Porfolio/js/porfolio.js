0d8d4268dfe1bb0e3c0628b8b8df1c4c6e2f8f26
Adrián Martín Muñoz <adrianmartinm04@gmail.com>
2024-09-15 16:39:00 +0200

    se termina por completo los estilos y el js del nav

--- Porfolio/js/porfolio.js
+++ Porfolio/js/porfolio.js
@@ -8,3 +8,32 @@
         }
         event.preventDefault();
     });
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let sections = document.querySelectorAll('.info'); 
+        let a = document.querySelectorAll('.profile__div--a');
+        let t='';
+        // Función callback para el IntersectionObserver
+        const verSection = (visual) => {
+            visual.forEach(view => {
+                if (view.isIntersecting) {
+                    // console.log(`Sección visible: ${view.target.classList[0]}`);
+                    t=view.target.classList[0];
+                } 
+                a.forEach(enlace => {
+                    if (enlace.classList.contains(t)) {
+                        enlace.parentNode.style.backgroundColor = "rgba(127, 255, 77, 0.6)";
+                    } else {
+                        enlace.parentNode.style.backgroundColor = ""; // Restablecer el color de los demás enlaces
+                    }
+                });
+            });
+        };
+    
+        // Crear un nuevo IntersectionObserver
+        const observer = new IntersectionObserver(verSection);
+    
+        // Observar cada sección
+        sections.forEach(section => observer.observe(section));
+    });
+    
